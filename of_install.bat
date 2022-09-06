19e01d1559a37f85a9d036259697e2a077fd379f
brysondev <bryson@brysondev.io>
2022-09-06 13:01:45 -0400

    v2.0.0 - Murse Wrapper

--- of_install.bat
+++ of_install.bat
@@ -1,26 +1,30 @@
-:: OF Install Script by Bryson :)
-::
+:: OF Install Wrapper Script.
+:: https://bry.so
+:: 
 @ECHO OFF
 cls
-title OF Installer Script for Windows v1.0
+title OF Installer Script for Windows
 
 echo (%TIME%) OF Install Script started.
 echo.
 echo Please make sure you have the required games installed before pressing continue!
 echo Team Fortress 2 and Source SDK 2013 Multiplayer are REQUIRED for this game to run.
 echo.
+echo Also expect this to freeze a bunch for a fresh install!!
+echo.
+PAUSE
 
 REG QUERY HKCU\SOFTWARE\Valve\Steam\Apps\440 > nul 2>&1
 if %ERRORLEVEL% EQU 0 (
 	REG QUERY HKCU\SOFTWARE\Valve\Steam\Apps\243750 > nul 2>&1
 	if %ERRORLEVEL% EQU 0 (
-		goto cont 
+		goto murse 
 	)
 )
 
 set /p gameInst="Games not detected. Do you want to install them now? (y/n): "
 If /i "%gameInst%"=="y" goto instgm
-If /i "%gameInst%"=="n" goto cont
+If /i "%gameInst%"=="n" goto murse
 
 :instgm
 echo.
@@ -33,26 +37,17 @@ echo.
 echo Installing Source SDK 2013 Multiplayer...
 start "" steam://install/243750
 echo.
-echo Once the game has downloaded successfully, hit enter.
+echo Once the game has downloaded successfully.
 pause
-goto cont 
+goto murse
+
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:cont
+:murse
 setlocal ENABLEEXTENSIONS
-set PATH_TORT=HKEY_LOCAL_MACHINE\SOFTWARE\TortoiseSVN
-set VALUE_TORT=ProcPath
-
-FOR /F "usebackq skip=2 tokens=1,2*" %%A IN (`REG QUERY %PATH_TORT% /v %VALUE_TORT% 2^>nul`) DO (
-    set TORT_REG_PATH=%%C
-)
-
-if not exist "%TORT_REG_PATH%" (
-	echo TortiseSVN not installed!
-	goto exitmain
-)
-
+set MURSE_PATH="%TEMP%\murse\"
 set PATH_STEAM=HKEY_CURRENT_USER\SOFTWARE\Valve\Steam
 set VALUE_STEAM=SourceModInstallPath
+set STEAM_EXE=SteamExe
 
 FOR /F "usebackq skip=2 tokens=1,2*" %%A IN (`REG QUERY %PATH_STEAM% /v %VALUE_STEAM% 2^>nul`) DO (
     set STEAM_REG_PATH=%%C
@@ -65,70 +60,117 @@ if not exist %STEAM_REG_PATH%\NUL > nul 2>&1(
 	)
 )
 
-:: :32bitSteam
-:: 01/07/21 - If you don't have any source engine games installed or attempted to have installed, keys don't generate. The paths are still fucked tho...
-::
-:: Steam fucked up the registry keys for 32 bit systems https://i.imgur.com/w816RTW.png
-:: Gotta use HKLM
-:: 
-:: set PATH_STEAM=HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam
-:: set VALUE_STEAM=InstallPath
-:: 
-:: FOR /F "usebackq skip=2 tokens=1,2*" %%A IN (`REG QUERY %PATH_STEAM% /reg:32 /v %VALUE_STEAM% 2^>nul`) DO (
-::     set STEAM_REG_PATH=%%C
-:: )
-:: 
-:: set "STEAM_REG_PATH=%STEAM_REG_PATH%\steamapps\sourcemods"
-:: 
-:: if not exist "%STEAM_REG_PATH%\" (
-:: 	echo Something broke...
-:: 	goto exitmain
-:: )
+if not exist "%MURSE_PATH%\murse.exe" (
+    echo Installing Murse CLI...
+    echo.
+    cd %TEMP%
+    md "murse"
+    cd "murse"
+    powershell -Command "Invoke-WebRequest https://dl.spiderden.net/murse/windows -Outfile murse.exe"
+)
 
-cd /D %STEAM_REG_PATH%
+echo Verifying Murse exists...
+cd /D "%MURSE_PATH%"
+murse.exe -h
+
+if errorlevel 1 (
+    echo Murse inaccessable. Please run this script as ADMIN!
+    goto exitmain
+)
+
+echo.
+echo Murse verified!
+:: cd /D %STEAM_REG_PATH%
 goto installOF
+
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :installOF
 echo.
 echo Installing Open Fortress...
 echo.
-echo.
-:: This is such a pain since by default users won't install command line application...
-if exist "open_fortress\.svn\wc.db" (
-	"%TORT_REG_PATH%" /command:cleanup /path:".\open_fortress" /cleanup /noui /noprogressui /breaklocks /refreshshell /externals /fixtimestamps /vacuum /closeonend:1
-	"%TORT_REG_PATH%" /command:update /path:".\open_fortress" /skipprechecks /closeonend:1
-	if not %ERRORLEVEL% == 0 (
-		echo Found an error. Attempting to relocate, update then cleanup...
-		echo.
-		"%TORT_REG_PATH%" /command:relocate /path:".\open_fortress" /closeonend:2
-		echo Please enter this url inside the box that just popped up: https://svn.openfortress.fun/svn/open_fortress
-		echo.
-		"%TORT_REG_PATH%" /command:update /path:".\open_fortress" /skipprechecks /closeonend:1
-	)
-	echo Cleaning up update...
-	echo.
-	"%TORT_REG_PATH%" /command:cleanup /path:".\open_fortress" /cleanup /noui /noprogressui /breaklocks /refreshshell /externals /fixtimestamps /vacuum /closeonend:2
-) ELSE (
-	echo Make sure you click ok for the popup as I cannot through this script...
-	echo.
-	"%TORT_REG_PATH%" /command:checkout /url:https://svn.openfortress.fun/svn/open_fortress /path:".\open_fortress" /closeonend:2
-	echo Cleaning up install...
-	echo.
-	"%TORT_REG_PATH%" /command:cleanup /path:".\open_fortress" /cleanup /noui /noprogressui /breaklocks /refreshshell /externals /fixtimestamps /vacuum /closeonend:2
+
+:: TODO: Possibly let them input threads, but honestly this should be fine for now...
+murse.exe upgrade "%STEAM_REG_PATH%\open_fortress"
+if %ERRORLEVEL% EQU 1 (
+    echo Something went wrong... 
+    goto exitmain
 )
+echo Validating just in case... 
+echo This will take a while...
+echo.
+murse.exe verify "%STEAM_REG_PATH%\open_fortress" -r
+if %ERRORLEVEL% EQU 1 (
+    echo Something went wrong... 
+    goto exitmain
+)
+
+tasklist /fi "ImageName eq steam.exe" /fo csv 2>NUL | find /I "steam.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    echo Steam is still running! Restarting it now...
+    taskkill /F /IM steam.exe
+)
+TIMEOUT /T 3
+tasklist /fi "ImageName eq steam.exe" /fo csv 2>NUL | find /I "steam.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    echo.
+    echo Steam is still running! We can't kill it for some reason. Exiting...
+    goto exitmain
+)
+
+FOR /F "usebackq skip=2 tokens=1,2*" %%A IN (`REG QUERY %PATH_STEAM% /v %STEAM_EXE% 2^>nul`) DO (
+    set STEAM_EXE_LOCATION=%%C
+)
+
+start "" "%STEAM_EXE_LOCATION%"
+
 goto finishMain
+
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :finishMain
+echo.
 echo ======================================
 echo Successfully installed Open Fortress!
 echo ======================================
-echo Please restart Steam (If you aren't updating) by clicking STEAM ^> ^EXIT at the top of the steam client, then restart!
 echo.
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMWWWNNXXKKKKKKKXXNNWWMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMWWNK0OkxdooolllllllloodxkO0KNWWMWMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMWNKOxdollllllllllcllllllllllllldxOKNWMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMWN0kdlllllllllllc::;,',;::cllllllllllldk0NWWWMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMWWKkdllcllllllcccc;'''::;'...';ccccllllllclldkKWWWMMMMMMMMMMMM
+echo MMMMMMMMMMMWWKxolllllllcc:;'..'..oKN0d;.....'..',:cclllllllokKWMMMMMMMMMMMM
+echo MMMMMMMMMMWXkolllllc:;,'''......,OWMKd;............',;:ccllllokXWMMMMMMMMMM
+echo MMMMMMMMMN0dllllll:'.':okOl..,cdONWMKd;.......:xo......':lllllld0NWMMMMMMMM
+echo MMMMMMMWXklllllllc,..dNWMW0k0XWWMMWMKd;.......;l:.......,clllllllkNWWMMMMMM
+echo MMMMMMWXxllllllll:'.;0MWMMMMMMMWNNWMKd:.................':llllllllxXWMMMMMM
+echo MMMMMWXxlclcllclc;..lXMMMMMWN0kOKNWMKd:..................;cclccclclxXWMMMMM
+echo MMMMMNklccccccccc,.'xWWWNKxoclkXWMMMKd;..................,ccccccccclkNMMMMM
+echo MMMMW0occccccccc:..;0MXd;''ckNWMMMMMKd;...................:ccccccccco0WMMMM
+echo MMMMNxcccccccccc,..lXMO,'l0NMWWMMMMMKd;............,,.....,ccccccccccxNMMMM
+echo MMMW0occccccccc:'.'xWWd,dNMMMMMMMMMMKd;............:c.....':cccccccccoKWMMM
+echo MMMWOl:cccccccc;..;0MXccKMMMMMMMMMMMKd;............cd,.....;c:ccccccclOWMMM
+echo MMMNkc:::::::::,..lNMOcxWMMMMMMMMMMMKd;............cO:.....,::c::::::ckWMMM
+echo MMMNkc:::::::::'.'xWWxoXMMMMMMMMMMMMKd;............cKo.....':::::::::ckNMMM
+echo MMMWkc::::::::;..;0MXdkWMMMMMMMMMMMMKd;............cXk'.....;::::::::cOWMMM
+echo MMMW0l::::::::,..lNMKkXMMMMMMMMMMMMMKd;...,:cc:;,'.lXK:.....,::::::::l0WMMM
+echo MMMMXd:::::::;'.'kWWKKWMMMMMMMMMWWMMKd:....,cdk0KK0KWNo.....';:::::;:dXMMMM
+echo MMMMWOc;;;;;;,..;KMWXNMMMMMMMMMMMWWXxc,........,cd0XWWx'.....,;;;;;;cOWMMMM
+echo MMMMMNx:;;;;;'..oNMWWWMMMMMWMWWXOdc,...............;lxd,.....';;;;;:xNMMMMM
+echo MMMMMWKo;;;;,..'kWWWMMMMMMWXOdc,..............................,;;;;oKWMMMMM
+echo MMMMMMWKo;;;,..:KWMMMWWXOdc,.....',,;;;,''....................,;;;oKWMMMMMM
+echo MMMMMMMWXd;,'..oNWWXOdc,.....'',,,,,,,,,,,,,''................',;dXWMMMMMMM
+echo MMMMMMMMWNk:..'oOdc,......',,,,,,,,,,,,,,,,,,,,,'..............:xNWMMMMMMMM
+echo MMMMMMMMMMW0c.........'',,,,,,,,,,,,,,,,,,,,,,,,,,,''.........c0WMMMMMMMMMM
+echo MMMMMMMMMMMMNk:...''',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'''..':kNMMMMMMMMMMMM
+echo MMMMMMMMMMMMMWNOo:,''''''''''''''''''''''''''''''''''''',:oONWMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMWKkl;''''''''''''''''''''''''''''''''';lkKWMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMWWKkdc;''''.''''''....''''..'''';cokKWWMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMWNKOxol:;,,'''''''''',;:loxOKNWMMMMMMMMMMMMMMMMMMMMMM
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXK00OOOkOOO00KXNWWMMMMMMMMMMMMMMMMMMMMMMMMMMM
 goto exitmain
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :exitmain
 echo.
-echo For any issues you are unsure about regarding the install, kindly ping bryson#1337 on the offical public Open Fortress Discord in #windows-troubleshooting.
-echo Discord: https://discord.gg/Jk3NUb7
+echo For any issues you are unsure about regarding the install, kindly ping bryson on the offical public Open Fortress Discord in #windows-troubleshooting.
+echo Discord: https://discord.gg/mKjW2ACCrm
 echo.
 PAUSE
